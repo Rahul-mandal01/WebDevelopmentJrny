60ad97e0d4ee7f9bc4342b5989ec554079899e8c
rahul.mandal01 <rahulra16809999@gmail.com>
2023-02-07 23:45:36 +0530

    updated_

---
+++ 07thFeb2023.html/JS BASIC/index.js
@@ -0,0 +1,43 @@
+// // line below is uesd to print console window
+// console.log('Namaste Duniya Version3');
+
+// let a='rahul';
+// console.log(a);
+
+// console.log(3==3);
+
+// let age=17;
+// let status=(age>=18)?'Yes Vote':'No vote';
+// console.log(status)
+
+// int marks=98;
+// if(marks>=90)
+//     console.log'(A');
+
+// let num=2;
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     default: console.log('C');
+// }
+
+// for(let i=0; i<5; i+=1){
+//     console.log(i);
+// }
+
+// let x=1;
+// while(x<=5){
+//     console.log(x)
+//     x++;
+// }
+
+// let y=0;
+// do{
+//     console.log(y);
+//     y++;
+// }
+// while(y<10);
+
+
