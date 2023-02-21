2203cb0f37aab6ad5c1a2b6fcbfb8430cbbc9f87
rahul.mandal01 <rahulra16809999@gmail.com>
2023-02-21 23:19:52 +0530

    _updatedJavascriptClass1

--- 07thFeb2023.html/JS BASIC/index.js
+++ 07thFeb2023.html/JS BASIC/index.js
@@ -33,5 +33,76 @@
 // a=5;
 // console.log(a);
 
+// console.log(5==3);
+
+// let age=27;
+// let status= (age>=18)? 'Yes Vote' : 'No Vote';
+
+// console.log(status);
+
+// let x=95;
+// let y=54;
+
+// if(x<50 && y>10)
+//     console.log('Jai Ho');
+// else
+//     console.log('Tumse Nhi Ho Payega');
+
+// let marks = 8;
+// if(marks >= 90)
+// {
+//     console.log('A');
+// }
+
+// else if(marks >= 80)
+// {
+//     console.log('B');
+// }
+
+// else if(marks >= 70)
+// {
+//     console.log('C');
+// }
+
+// else if(marks >= 60)
+// {
+//     console.log('D');
+// }
+
+// else
+// {
+//     console.log('FAIL');
+// }
+
+// let num =1;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+// for(let i=1; i<=5; i+=1){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log(i)
+//     i+=2;
+// }
+
+
+// let i= 5;
+// do{
+//     console.log(i);
+//     i+=10;
+// }
+// while(i<=100)
+
 
 
