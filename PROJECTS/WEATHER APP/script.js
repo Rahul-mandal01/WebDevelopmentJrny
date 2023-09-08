44380d96fd1b781d2531e662a57a9eddc3b8a390
Your Name <rahulra16809999@gmail.com>
2023-09-08 13:51:55 +0530

    update html file

--- PROJECTS/WEATHER APP/script.js
+++ PROJECTS/WEATHER APP/script.js
@@ -1,100 +1,40 @@
-console.log('hello jii');
+const userTab = document.querySelector("[data-useWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector("[weather-container]");
 
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+// initially variables need??
+
+let currentTab = userTab;
 const API_KEY = "416da1ee2c3d4102062294581fa1f086";
+currentTab.classList.add("current-tab");
 
-function renderWeatherInfo(data){
-    let newPara = document.createElement('p');
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
-
-    document.body.appendChild(newPara);
-}
-
-async function fetchWeatherDetails(){
-
-    try{
-    // let latitude = 15.33;
-    // let longitude= 74.08;
-
-    let city = "kolkata";
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`); 
-
-    const data = await response.json();
-    console.log("weather data:-> ", data);
-
-    // let newPara = document.createElement('p');
-    // newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
-
-    // document.body.appendChild(newPara);
-
-    renderWeatherInfo(data);
-    }
-    catch(err){
-        // handle the error here
-    }
-
-}
-
-async function getCustomWeatherDetails(){
-
-    try{
-        let latitude = 22.572;
-        let longitude= 88.3573;
-    
-        let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    
-        let data = await result.json();
-    
-        console.log(data);
-        renderWeatherInfo(data);
-    }
-
-    catch(err){
-        // handle the error here
-        console.log("Error found", err );
-    }
-
-}
+// a work needs to be done more
 
 function switchTab(clickedTab){
-
-    apiErrorContainer.classList.remove("active");
-
-    if(clickedTab !== currentTab){
+    if(clickedTab != currentTab){
         currentTab.classList.remove("current-tab");
         currentTab = clickedTab;
         currentTab.classList.add("current-tab");
-    }
 
-    if(! searchForm.classList.contains("active")){
-        userInfoContainer.classList.remove("active");
-        grantAccessContainer.classList.remove("active");
-        searchForm.classList.add("active");
-    }
-
-    else{
-        searchForm.classList.remove("active");
-        userInfoContainer.classList.remove("active");
-        // getFromSessionStorage();
-    }
-
-    // console.log("Current Tab", currentTab);
-}
-
-function getLocation(){
-    if(navigator.geoLocation){
-        navigator.geoLocation.getCurrentPosition(showPosition);
-    }
-    
-    else{
-        console.log("Geolocation is not supported by this browser");
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
     }
 }
 
-function showPosition(position){
-    let lat = position.coords.latitude;
-    let longi = position.coords.longitude;
+userTab.addEventListener("click", () => {
+    // pass clicked tab as input parameter
+    switchTab{userTab};
+});
 
-    console.log(lat);
-    console.log(longi);
-}
+searchTab.addEventListener("click", () => {
+    // pass clicked tab as input parameter
+    switchTab{searchTab};
+});
