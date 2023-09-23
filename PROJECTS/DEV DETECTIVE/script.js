df774225bd1895ef3cc68a0936dbb41ea8f0a3fa
Your Name <rahulra16809999@gmail.com>
2023-09-23 21:22:41 +0530

    html file upload

---
+++ PROJECTS/DEV DETECTIVE/script.js
@@ -0,0 +1,24 @@
+
+
+
+const lightMode = document.querySelector("[data-lightMode]");
+const darkMode = document.querySelector("[data-darkMode]");
+const searchForm = document.querySelector("[data-searchForm]");
+const searchInput = document.querySelector("[data-searchInput]");
+const searchBtn = document.querySelector("[data-searchBtn]");
+const userImage = document.querySelector("[data-userImage]");
+const userName = document.querySelector("[data-username]");
+const userJoinedDetail = document.querySelector("[data-joinedDetail]");
+const userId = document.querySelector("[data-userId]");
+const userBio = document.querySelector("[data-userBio]");
+const userRepo = document.querySelector("[data-userRepo]");
+const userFollowers = document.querySelector("[data-userFollowers]");
+const userFollowing = document.querySelector("[data-userFollowing]");
+const userLocation = document.querySelector("[data-userLocation]");
+const userTwitter = document.querySelector("[data-userTwitter]");
+const userCompany = document.querySelector("[data-userCompany]");
+
+
+
+
+
