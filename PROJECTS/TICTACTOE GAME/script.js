1831b2dc25139f748e7137c32dee01e66aaea1c4
Your Name <rahulra16809999@gmail.com>
2023-10-21 20:21:38 +0530

    update script.js

---
+++ PROJECTS/TICTACTOE GAME/script.js
@@ -0,0 +1,37 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPosition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+// creating a function to initialise the game
+function initGame(){
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        handleClick(index);
+    });
+});
+
+
+
