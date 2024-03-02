a30ef568e15f58cde9db063151a51c73230b1bc5
Your Name <rahulra16809999@gmail.com>
2024-03-02 19:12:27 +0530

    update package.json

--- REACT/INSTALLATION/react-tailwind-css-starter-pack-main/src/App.js
+++ REACT/INSTALLATION/react-tailwind-css-starter-pack-main/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-
+const randomNum = require('random-num')
 function App() {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div className="App">
+        <header className="App-header">
+            <h1>{randomNum(1,100)}</h1>
+        </header>
+    </div>
   );
 }
 
