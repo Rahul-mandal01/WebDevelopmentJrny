0bb98627e7de17fdf85d7b4dc3c27a3c24046a61
Your Name <rahulra16809999@gmail.com>
2024-03-04 21:35:22 +0530

    Update Item.js

--- REACT/REACT1/ReactFolio/demoshopapp/src/components/Item.js
+++ REACT/REACT1/ReactFolio/demoshopapp/src/components/Item.js
@@ -1,9 +1,14 @@
 
 import './Item.css'
 
-function Item() {
-    const itemName = "Nirma";
-    return(<p className="nirma">{itemName}</p>);
+function Item(props) {
+    const itemName = props.name;
+    return(
+    <div>
+        <p className="nirma">{itemName}</p>
+        {props.children}
+    </div>
+    );
 }
 
 export default Item;
