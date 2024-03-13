3017ed9091ff02e148c40194215616f453a2006c
Your Name <rahulra16809999@gmail.com>
2024-03-13 13:47:17 +0530

    Create App.js

---
+++ REACT/REACT2/DEMOSHOPAPPLICATION/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Products from './components/Products';
+
+const App = () => {
+
+  const products = [
+    {
+      id:'p1',
+      title:'Nirma',
+      amount:100,
+      date: new Date(2021, 8, 10),
+    },
+
+    {
+      id:'p2',
+      title:'Surf Excel',
+      amount:200,
+      date: new Date(2021, 2, 5),
+    },
+
+    {
+      id:'p3',
+      title:'Tide',
+      amount:130,
+      date: new Date(2021, 12, 28),
+    },
+
+    {
+      id:'p4',
+      title:'Maggi',
+      amount:450,
+      date: new Date(2021, 10, 8),
+    },
+
+    
+  ];
+
+  return(
+    <div>
+      <Products items={products}/>
+    </div>
+  )
+
+}
+
+export default App;
