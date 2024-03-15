76f23e88867da25dc44054c093319c56589141e2
Your Name <rahulra16809999@gmail.com>
2024-03-15 20:34:31 +0530

    Update ProductForm.js

--- REACT/REACT2/DEMOSHOPAPPLICATION/src/components/ProductForm.js
+++ REACT/REACT2/DEMOSHOPAPPLICATION/src/components/ProductForm.js
@@ -3,19 +3,23 @@ import './ProductForm'
 function ProductForm(){
 
     return(<form>
-        <div>
-            <label className='new-product_title'>Title</label>
-            <input type='text'></input>
+
+        <div className='new-product_controls'>
+            <div className='new-product_control'>
+                <label>Title</label>
+                <input type='text'></input>
+            </div>
+
+            <div className='new-product_control'>
+                <label>Date</label>
+                <input type='date' min='2023-01-01' max='2024-12-12'></input>
+            </div>
+        
+            <div className='new-product_button'>
+                <button type='submit'>Add Product</button>
+            </div>
         </div>
 
-        <div>
-            <label className='new-product_date'>Date</label>
-            <input type='date' min='2023-01-01' max='2023-12-12'></input>
-        </div>
-        
-        <div className='new-product_button'>
-            <button type='submit'>Add Product</button>
-        </div>
     </form>)
 }
 
