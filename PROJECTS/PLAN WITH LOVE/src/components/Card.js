20630d7bae99ceb988f2cb582fb26afa588267e9
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-03-22 10:06:58 +0530

    Update Card.js

--- PROJECTS/PLAN WITH LOVE/src/components/Card.js
+++ PROJECTS/PLAN WITH LOVE/src/components/Card.js
@@ -1,5 +1,7 @@
+import React, {useState} from "react";
 
-function Card({id, image,info,price,name}){
+
+function Card({id, image,info,price,name, removeTour}){
 
     const[readmore, setReadmore] = useState(false);
 
@@ -9,23 +11,29 @@ function Card({id, image,info,price,name}){
         setReadmore(!readmore);
     }
 
-    return(
 
-            <div className="card">
-                <img src={image} className="image" ></img>
+
+    return(
+        <div className="card">
+            <img src={image} alt="" className="image" ></img> 
+            <div className="tour-info">
                 <div className="tour-details" >
                     <h4 className="tour-price">{price}</h4>
                     <h4 className="tour-name">{name}</h4>
                 </div>
-
-               <div className="description" >
-                    {description}
-                    <span className="read-more" onClick{readmoreHandler}>
-                        {readmore ? `show less`: `read more`}
-                    </span>
-               </div>
+                <div className="description" >
+                        {description}
+                        <span className="read-more" onClick={readmoreHandler}>
+                            {readmore ? `show less`: `read more`}
+                        </span>
+                </div>
             </div>
-        );
+            <button className="btn-red" onClick={() => removeTour(id)}>
+                Not interested
+            </button>    
+      </div>
+    );
+
 }
 
 export default Card;
