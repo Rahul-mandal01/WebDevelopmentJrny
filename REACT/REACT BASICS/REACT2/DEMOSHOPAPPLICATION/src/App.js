9221c624f3d8482e5dda3c0fe15a697432304531
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-19 22:04:52 +0530

    Updating DEMOSHOPAPPLICATION

---
+++ REACT/REACT BASICS/REACT2/DEMOSHOPAPPLICATION/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+
+const App = () => {
+
+  const products = [
+    {
+      id:'p1',
+      title:'Nirma',
+      amount:100,
+      date: new Date(2021, 8, 10),
+    },
+
+    {
+      id:'p2',
+      title:'Surf Excel',
+      amount:200,
+      date: new Date(2021, 2, 5),
+    },
+
+    {
+      id:'p3',
+      title:'Tide',
+      amount:130,
+      date: new Date(2021, 12, 28),
+    },
+
+    {
+      id:'p4',
+      title:'Maggi',
+      amount:450,
+      date: new Date(2021, 10, 8),
+    },
+
+    
+  ];
+
+  function printProductData(data){
+    console.log("I am inside App.js");
+    console.log(data);
+  }
+
+  return(
+    <div>
+      <NewProduct onPrintProduct = {printProductData}/>
+      <Products items={products}/>
+    </div>
+  )
+
+}
+
+export default App;
