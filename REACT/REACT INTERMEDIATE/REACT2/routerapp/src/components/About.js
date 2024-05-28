844fd8fb1d7a32d780442ecf80b5fd26509ae02c
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-28 09:46:35 +0530

    Updating About.js

---
+++ REACT/REACT INTERMEDIATE/REACT2/routerapp/src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+ const About = ()=> {
+
+    const navigate = useNavigate();
+
+
+    function clickHandler(){
+        // move to labs page\
+        navigate("/labs");
+    }
+
+
+
+  return (
+    <div>
+        <div>This is About page</div>
+        <button onClick={clickHandler} >Move to Labs Page </button>
+    </div>
+  )
+}
+
+export default About;
+
+
